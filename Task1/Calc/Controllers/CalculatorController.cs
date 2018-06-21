b2f108733d16ffdc32bfd4cd21c336e65e772b9e
Laxusjee <dx98@bk.ru>
2018-06-21 19:38:15 +0400

    Task1

---
+++ Task1/Calc/Controllers/CalculatorController.cs
@@ -0,0 +1,64 @@
+﻿namespace Calc.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Provides calculator functions 
+    /// </summary>
+    public class CalculatorController : Controller
+    {
+        /// <summary>
+        /// Calculating based on the object properties
+        /// </summary>
+        /// <param name="calculator">Calculating object</param>
+        /// <param name="logs">Previous logs</param>
+        /// <returns>View with previous logs</returns>  
+        public ActionResult Calculator(Models.CalculatorModel calculator = null, string logs = null)
+        {
+            if (calculator != null)
+            {
+                if (ModelState.IsValid)
+                {
+                    if (calculator.SelectedMethod == "Sum")
+                    {
+                        int res = this.Sum(calculator.X, calculator.Y);
+                        ViewBag.logs = logs + $"Дата и время: {DateTime.Now.ToString()}     {calculator.X} + {calculator.Y} = {res} \n";
+                    }
+
+                    if (calculator.SelectedMethod == "Prod")
+                    {
+                        int res = this.Prod(calculator.X, calculator.Y);
+                        ViewBag.logs = logs + $"Дата и время: {DateTime.Now.ToString()}     {calculator.X} * {calculator.Y} = {res} \n";
+                    }
+
+                    return this.View();
+                }
+                else
+                {
+                    ViewBag.NotValidMessage = "Данные не прошли валидацию";
+                    ViewBag.logs = logs;
+                    return this.View();
+                }
+            }
+
+            return this.View();
+        }
+
+        /// <summary>
+        /// Summing two numbers
+        /// </summary>
+        /// <param name="x">first number</param>
+        /// <param name="y">second number</param>
+        /// <returns>Sum of numbers</returns>
+        public int Sum(int x, int y) => x + y;
+
+        /// <summary>
+        /// Multiplies two numbers
+        /// </summary>
+        /// <param name="x">first number</param>
+        /// <param name="y">second number </param>
+        /// <returns>Product of numbers</returns>
+        public int Prod(int x, int y) => y * x;
+    }
+}
---
+++ Task1/Calc/Models/CalculatorModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Calc.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Calculation object
+    /// </summary>
+    public class CalculatorModel
+    {
+        /// <summary>
+        /// Gets or Sets value of fist number 
+        /// </summary>
+        [Required]
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets value of first number
+        /// </summary>
+        [Required]
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Name of the selected method
+        /// </summary>
+        public string SelectedMethod { get; set; }
+    }
+}
